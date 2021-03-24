963f2c4c667067907a725cb79078489dcb696e52
vicc23@gmail.com <vicc23@gmail.com>
2021-03-24 11:36:19 -0600

    Added search basic features

--- script.js
+++ script.js
@@ -9,3 +9,25 @@
  * Victor C.
  */
 
+// Elements shortcut
+const btn = document.getElementById('search-btn');
+const icon = document.getElementById('icon-search')
+const input = document.getElementsByTagName('input')[0];
+
+//Event listener click search
+btn.addEventListener('click', search);
+icon.addEventListener('click', search);
+
+//Event listener press keyboar enter
+input.addEventListener('keyup', (e) => {
+    if (e.code === 'Enter') search();
+});
+
+//Function that opens search in existing tab
+function search() {
+    const qry = input.value
+    if (qry != "") {
+        console.log(qry);
+        window.location.replace(`https://www.google.com/search?q=${qry}`);
+    } 
+}
